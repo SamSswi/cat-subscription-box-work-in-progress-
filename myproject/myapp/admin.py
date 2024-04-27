00b8a75ecf4ed43fb47a07e035b6376d99b6937b
SamSswi <sadovoisamuel7@gmail.com>
2024-04-27 14:20:26 +0200

    Registered models in admin interface

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile, Order, Product, OrderLine, SubscriptionPlan, Subscription
 
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(OrderLine)
+admin.site.register(SubscriptionPlan)
+admin.site.register(Subscription)
